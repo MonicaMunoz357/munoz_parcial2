bebc41aec5cdecaec3a7a357222e866ffb4b5007
Mónica Mariana Muñoz Torres <128828587+MonicaMunoz357@users.noreply.github.com>
2025-07-03 15:39:20 -0600

    Primeros cambios

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './monica.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Monica Mariana Muñoz Torres  - EXAMEN PARCIAL 2
+        </p>
+        <a
+          className="App-link"
+          href="https:www.utd.edu.mx"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          UNIVERSIDAD TECNOLÓGICA DE DURANGO - CLICK PARA VISITAR EL SITIO
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
